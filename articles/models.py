3544e43eb907886aff5ad1c7eb0f94136a0d1beb
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-04-29 11:52:52 +0200

    add an image_tag to view the image in the admin view

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import mark_safe
+
 
 class Article(models.Model):
     """
@@ -7,6 +9,13 @@ class Article(models.Model):
     title = models.CharField('title', max_length=50)
     description = models.TextField('description', max_length=500, default='')
     image_url = models.URLField(null=True)
+
+    def image_tag(self):
+        # to show the image in  the admin site
+        return mark_safe(f'<img src="{self.image_url}" alt="Image not found" \
+            style="border: 1px solid #000; max-width:250px; max-height:250px;"/>')
+
+    image_tag.short_description = 'Image'
     
     def __str__(self):
         return self.title
