4efa267f8cddc6102aeee61fd1229f3c815298a5
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-04-29 11:53:50 +0200

    customizing the admin view with search field and show the image_tag

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Article
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ['title',]
+    fields = ("title", "image_tag", "description", "image_url",)
+    readonly_fields = ('image_tag',)
 
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
