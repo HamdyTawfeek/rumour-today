92e7b23f4db36bb646174c53c98258e498c43d4a
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-04-29 18:13:18 +0200

    added a contact form for sending email

---
+++ articles/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100, required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+    sender = forms.EmailField(required=True)
