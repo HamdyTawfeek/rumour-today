69bd21a116113264fb817c32ced483729f361722
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-04-29 22:47:15 +0200

    raise the max limit to 200

--- articles/forms.py
+++ articles/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100, required=True)
+    subject = forms.CharField(max_length=200, required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
     sender = forms.EmailField(required=True)
