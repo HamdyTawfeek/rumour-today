7a1429443d32674bbc7bdbfdca90e367b12c2684
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-04-29 11:33:05 +0200

    Added Article data model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    """
+    Article model
+    """
+    title = models.CharField('title', max_length=50)
+    description = models.TextField('description', max_length=500, default='')
+    image_url = models.URLField(null=True)
+    
+    def __str__(self):
+        return self.title
