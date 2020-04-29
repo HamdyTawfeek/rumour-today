acc580cbf0e4586b598d0e682a2e014ea2ac8ce0
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-04-29 14:23:11 +0200

    simple view for the home page to list articles with images only

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from articles.models import Article
 
-# Create your views here.
+def index(request):
+    context = {
+        'articles': Article.objects.all(),
+    }
+    return render(request, 'articles/articles_list.html', context)
