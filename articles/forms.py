ed171c65cd9310e3b78833778a879714543df638
Hamdy <hamdy.t@trufla.com>
2020-04-30 10:43:18 +0200

    add a doc string for contact form

--- articles/forms.py
+++ articles/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 class ContactForm(forms.Form):
+    '''
+    Email contact form 
+    '''
     subject = forms.CharField(max_length=200, required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
     sender = forms.EmailField(required=True)
