c0b0c506351e131db35df37a9ac07933e4eb92ab
Hamdy <hamdy.t@trufla.com>
2020-04-30 06:04:32 +0200

    add a field for contact form recipient

--- articles/forms.py
+++ articles/forms.py
@@ -4,3 +4,4 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=200, required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
     sender = forms.EmailField(required=True)
+    recipient = forms.EmailField(required=True)
